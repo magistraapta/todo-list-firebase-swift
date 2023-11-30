f4d73ce72eaeb712b60c507de826292a6f2cab88
Magistra <magistraagis@gmail.com>
2023-11-30 11:57:46 +0800

    add sign up function and auth model

--- todo-list-firebase/AuthManager.swift
+++ todo-list-firebase/AuthManager.swift
@@ -8,16 +8,28 @@
 import Foundation
 import FirebaseAuth
 
+struct AuthDataResultModel{
+    var uid: String
+    var email: String?
+    var photoUrl: String?
+    
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email
+        self.photoUrl = user.photoURL?.absoluteString
+    }
+}
+
 class AuthManager{
-    let shared = AuthManager()
+    static let shared = AuthManager()
     
     init(){}
     
     let firebaseAuth = Auth.auth()
     
-    func createNewUser(email: String, password: String) async throws {
+    func createNewUser(email: String, password: String) async throws -> AuthDataResultModel {
         
-        try await firebaseAuth.createUser(withEmail: email, password: password)
-        
+        let authDataResult = try await firebaseAuth.createUser(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
     }
 }
